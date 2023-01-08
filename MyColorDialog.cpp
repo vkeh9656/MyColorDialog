79b7b2f7c4c48a4d35cf64fbe3cef35758820065
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-01-08 11:22:32 +0900

    초기 커밋.

---
+++ MyColorDialog.cpp
@@ -0,0 +1,47 @@
+﻿
+// MyColorDialog.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MyColorDialog.h"
+#include "MyColorDialogDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMyColorDialogApp
+
+BEGIN_MESSAGE_MAP(CMyColorDialogApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CMyColorDialogApp 생성
+
+CMyColorDialogApp::CMyColorDialogApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CMyColorDialogApp 개체입니다.
+
+CMyColorDialogApp theApp;
+
+
+// CMyColorDialogApp 초기화
+
+BOOL CMyColorDialogApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	CMyColorDialogDlg dlg;
+	dlg.DoModal();
+	
+	return FALSE;
+}
+
